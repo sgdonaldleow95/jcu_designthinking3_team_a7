854dba4583e042e78866b6c667e3ea8692da4cba
Marcus <jianjiemarcus.wee@my.jcu.edu.au>
2018-12-12 14:38:21 +0800

    Third Commit Add database connection. Add doLogin, doRegister & register empty files.
    Update navigation to a file to be standardized

--- includes/footer.php
+++ includes/footer.php
@@ -1,2 +1,1 @@
-<br><br>
 Develop by Marcus Wee, Donald Leow, Yash & Joshua |  2018&copy;
--- index.php
+++ index.php
@@ -6,48 +6,50 @@
 		<link href="css/external.css" rel="stylesheet"/>
         <link href="css/nav.css" rel="stylesheet"/>
         <link href="css/media.css" rel="stylesheet"/>
-
 </head>
 
 <body>
 	<header>
-	<img id="img1" src="images/banner.jpg" height="100%" width="100%">
+	    <img id="img1" src="images/banner.jpg" width="100%">
 	</header>
 <nav>
-	<ul>
-		<li><a href="index.php">Home</a></li>
-		<li><a href="#">Browse Jobs</a></li>
-		<li><a href="#">FAQ</a></li>
-		<li><a href="#">About Us</a></li>
-		<li><a href="#">Contact Us</a></li>
-	</ul>
+    <?php include('includes/nav.php') ?>
 </nav>
 <section>
 	<div id="content">
-	<br>
-	<center><h3><u>Jobs Listed here</u></h3></center>
-	<br>
+	    <br>
+        <center><h3><u>JCU Singapore Job Portal</u></h3></center>
+        <br>
+        <p>Searching for a job or looking for potential employees?</p>
 	</div>
 <aside>
-	<center>
-	<h3><font color="#FFFFFF">Portal Credentials</font></h3>
+    <center><h3><font color="#000">Portal Credentials</font></h3>
 	<hr>
 	<br>
-	<form id='login' action='doLogin.php' method='post'>
-		<input type='text' name='username' id='username' maxlength='50' placeholder='Username'>
-		<input type='password' name='password' id='password' maxlength='50' placeholder='Password'>
-		<br><br>
-		<button type="submit">Login</button> <button type="submit">Forget Password</button>
-		<br><br>
-		<button type="submit">Registration</button>
-	</form>
-	<br>
+        <form name="login" method="post" action="doLogin.php">
+            <fieldset>
+                <table>
+                    <tr>
+                        <td><font color="black">Username :</font></td>
+                        <td><input name="username" type="text" size="20" placeholder="Username" required></td>
+                    </tr>
+                    <tr><td><font color="black">Password :</font></td>
+                        <td> <input name="password" type="password" placeholder="password" required></td>
+                    </tr>
+                </table>
+                <button class="button" input name="submit" type="submit"><b>Log In</b></button>
+                <a href="registration.php"><button class="button" input type="button"><b>Register</b></button></a><br>
+                <a href="forgetPassword.php"><button class="button" input type="button"><b>Forget Password?</b></button></a>
+            </fieldset>
+        </form>
+
+        <br>
 	<br>
 	<br>
 	</center>
 </aside>
 </section>
-<footer>
+<footer id="footer">
 	<?php include('includes/footer.php') ?>
 </footer>
 </body>
