4dcdc3c14c0352e95a5e42d08b2684ea023cc283
Reginald96 <44828509+Reginald96@users.noreply.github.com>
2018-12-27 19:04:38 +0800

    Add files via upload

---
+++ dt3_assign1/doLogout.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+$_SESSION = array();
+session_destroy();
+?>
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>JCU Singapore Job Portal</title>
+    <link href="css/external.css" rel="stylesheet"/>
+    <link href="css/nav.css" rel="stylesheet"/>
+    <link href="css/media.css" rel="stylesheet"/>
+</head>
+<body>
+
+<section>
+    <article>
+        <center>
+            <?php header("Location: index.php"); ?>
+        </center>
+    </article>
+</section>
+<footer id="footer">
+    <?php include('includes/footer.php')?>
+</footer>
+</body>
+</html>
---
+++ dt3_assign1/includes/footer.php
@@ -0,0 +1,1 @@
+Develop by Marcus Wee, Donald Leow, Yash & Joshua |  2018&copy;
