d08b383ba2d7ee72166ef5dcf2728077a5b09766
Reginald96 <reginaldjoshua.young@my.jcu.edu.au>
2018-12-28 14:09:21 +0800

    company information

--- companyProfilePage.php
+++ companyProfilePage.php
@@ -10,10 +10,63 @@
 </head>
 <body>
 <header>
-    <a href="index.php"><img id="img1" src="images/logo.jpeg" height="120" width="250"></a>
+    <img id="img1" src="images/banner.jpg" width="100%">
 </header>
 <nav>
     <?php include('nav.php') ?>
 </nav>
+<section>
+    <div id="content">
+        <center>
+            <h3>Company Profile</h3>
+            <br>
+            <form name="comapnyProfile" method="post" action="doUpdateProfileCompany.php" ENCTYPE="multipart/form-data">
+                <table>
+                    <tr>
+                        <td>Company ID:</td><td><input type='text' name='companyid' placeholder="Company ID Number" required></td>
+                    </tr>
+                    <tr>
+                        <td>Company Name:</td><td><input type='text' name='companyname' placeholder='Name of Company' required></td>
+                    </tr>
+                    <tr>
+                        <td>Location</td><td><input type="text" name="location" placeholder="Company Address" required></td>
+                    </tr>
+                    <tr>
+                        <td>Industry</td><td><input type="text" name="industrytype" placeholder="Industry Type" required> </td>
+                    </tr>
+                    <tr>
+                        <td>Company Description</td><td><input type="text" name="description" placeholder="About the company" required> </td>
+                    </tr>
+                    <tr>
+                        <td>Company Website</td><td><input type="email" name="email" placeholder="Company email address" required> </td>
+                    </tr>
+                    <tr>
+                        <td>Short description<</td><td><textarea rows="4" cols="50"></textarea></td>
+                    </tr>
+                    <tr>
+                        <td></td><td><input class="button" input type="file" id="resume" name="document"></td>
+                    </tr>
+                </table>
+                <input type="hidden" id="studentid" name="studentid" value= "<?php echo $_SESSION['id'] ?>" >
+                <button class="button" input name="submit" type="submit">Submit</button>
+
+            </form>
+        </center>
+        <br>
+    </div>
+
+    <aside>
+        <center>
+            <?php include('sidepanel.php') ?>
+
+        </center>
+    </aside>
+</section>
+
+
+
+<footer id="footer">
+    <?php include('includes/footer.php') ?>
+</footer>
 </body>
 </html>
